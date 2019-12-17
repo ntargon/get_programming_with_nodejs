e4e2609e25b44399f688b638c208760d9ff78630
Naoki Tokunaga <argon19951128@gmail.com>
2019-12-17 10:46:54 +0900

    lesson 11 done

--- app/express_templates/main.js
+++ app/express_templates/main.js
@@ -4,13 +4,19 @@ const express = require("express"),
 	app = express();
 
 const homeController = require("./controllers/homeController");
+const errorController = require("./controllers/errorController");
 const layouts = require("express-ejs-layouts");
 
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
 app.use(layouts);
+app.use(express.static("public"));
+
 
 app.get("/name/:myName", homeController.respondWithName);
+
+app.use(errorController.respondNoResourceFound);
+app.use(errorController.respondInternalError);
 
 app.listen(app.get("port"), () => {
 	console.log(`Server running at http://localhost:${app.get("port")}`);
